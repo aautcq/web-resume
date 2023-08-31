1230f57d5e2899fa294e5020a78228e1a7096fc4
aautcq <adrien.autricque@gmail.com>
2023-08-31 16:11:34 +0200

    Improve pdf generation script: external stylesheet, automatic detection of page dimensions and title. Also improve resume content

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-const RESUME_HEIGHT = 1331;
-
 (async () => {
   // Create a browser instance
   const browser = await puppeteer.launch({ headless: 'new' });
@@ -10,18 +8,31 @@ const RESUME_HEIGHT = 1331;
   // Create a new page
   const page = await browser.newPage();
 
-  // Get HTML content from HTML file
+  // Set page content (HTML and CSS)
   const html = fs.readFileSync('index.html', 'utf-8');
   await page.setContent(html, { waitUntil: 'networkidle0' });
+  await page.addStyleTag({ path: 'style.css' });
 
   // To reflect CSS used for screens instead of print
   await page.emulateMediaType('screen');
 
+  // Wait for the fonts to be loaded
+  await page.evaluateHandle('document.fonts.ready');
+
+  // Get page dimensions
+  const elem = await page.$('.resume');
+  const { height, width } = await elem.boundingBox();
+
+  // Get page title
+  const title = await page.title();
+  const formattedTitle = title.replace(/\s/g, '-').toLowerCase() + '.pdf';
+  console.log(formattedTitle);
+
   // Downlaod the PDF
   await page.pdf({
-    path: 'CV-adrien-autricque-fr.pdf',
-    width: 800,
-    height: RESUME_HEIGHT,
+    path: formattedTitle,
+    width,
+    height,
     printBackground: true
   });
 
