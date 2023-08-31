cdf43419888eb2b04087757bc12e8422ac1dc69f
aautcq <adrien.autricque@gmail.com>
2023-08-31 11:56:31 +0200

    PDF build

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # web-resume
-HTML version of resume
+
+HTML version of resume. You can either use the index.html file directly or generate a PDF using the following command:
+
+```bash
+$ npm i
+$ npm run build
+```
---
+++ index.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+  // Create a browser instance
+  const browser = await puppeteer.launch({ headless: 'new' });
+
+  // Create a new page
+  const page = await browser.newPage();
+
+  // Get HTML content from HTML file
+  const html = fs.readFileSync('index.html', 'utf-8');
+  await page.setContent(html, { waitUntil: 'networkidle0' });
+
+  // To reflect CSS used for screens instead of print
+  await page.emulateMediaType('screen');
+
+  // Downlaod the PDF
+  await page.pdf({
+    path: 'CV-adrien-autricque-fr.pdf',
+    width: 800,
+    height: 1331,
+    printBackground: true
+  });
+
+  // Close the browser instance
+  await browser.close();
+})();
